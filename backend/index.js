f8ac2c728c68af126a113d925b5faf56468bc264
juansolar <juansolar@unicauca.edu.co>
2020-07-01 11:20:29 -0500

    instalacion de nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('server on port 3000');
+//settings
+app.set('port', process.env.PORT ||3000);
+
+//middlewares
+
+//routes
+
+
+//star server
+app.listen(app.get('port'), () => {
+    console.log('server on port ', app.get('port'));
 })
