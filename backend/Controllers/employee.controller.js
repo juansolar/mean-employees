8a21c2a08814460c1d05f548c05aa0cfd2761482
juansolar <juansolar@unicauca.edu.co>
2020-07-02 20:29:17 -0500

    creación de las funciones del CRUD

--- backend/Controllers/employee.controller.js
+++ backend/Controllers/employee.controller.js
@@ -1,13 +1,43 @@
+const Employee = require('../models/employee');
+//const { restart } = require('nodemon');
+
 const employeeCtrl = {};
 
-employeeCtrl.getEmployee = (req, res) =>{
+employeeCtrl.getEmployees = async (req, res) =>{
+    const employees = await Employee.find(); //await en el caso de una busqueda muy grande
+    res.json(employees);
+};
+
+employeeCtrl.createEmployee = async (req, res) => {
+    const employee = new Employee(req.body);
+    await employee.save();
     res.json({
-        status : 'get employee'
+        status : "Employee saved"
     });
+};
+
+
+employeeCtrl.getEmployee = async (req,res) => {
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee);
+};
+
+employeeCtrl.editEmployee = async (req, res) => {
+    const { id } = req.params;
+    const employee ={
+        name : req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    };
+    await Employee.findByIdAndUpdate(id, {$set: employee}, {new: true} ); //new true=> en el caso que no existe lo crea
+    res.json({status: 'updated'});
+};
+
+employeeCtrl.deleteEmployee = async(req, res) => {
+    await Employee.findByIdAndRemove(req.params.id);
+    res.json({status: 'deleted'});
 }
 
-employeeCtrl.createEmployee = function(){
-
-}
 
 module.exports = employeeCtrl;
--- backend/routes/employee.routes.js
+++ backend/routes/employee.routes.js
@@ -4,7 +4,10 @@ const router = express.Router(); //objeto el cual le agregamos propiedades (ruta
 
 const employeeCtrl = require('../Controllers/employee.controller');
 
-router.get('/', employeeCtrl.getEmployee);
-
+router.get('/', employeeCtrl.getEmployees);
+router.post('/', employeeCtrl.createEmployee);
+router.get('/:id', employeeCtrl.getEmployee);
+router.put('/:id', employeeCtrl.editEmployee);
+router.delete('/:id', employeeCtrl.deleteEmployee);
 
 module.exports = router;
