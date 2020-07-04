544597d8dfc0f53c39d8a8bd9b7c84276b3a652f
juansolar <juansolar@unicauca.edu.co>
2020-07-04 15:58:52 -0500

    creacion de las rutas con el backend, formulario y agregar empleados

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//nuevis
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './componets/employees/employees.component';
+//import { SegundoComponent } from './componets/segundo/segundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    //SegundoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/componets/employees/employees.component.ts
+++ frontend/src/app/componets/employees/employees.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+import { Employee } from '../../models/employee';
+import { NgForm } from '@angular/forms';
+
+declare var M: any;
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+  styleUrls: ['./employees.component.css'],
+  providers: [EmployeeService]
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public employeeService: EmployeeService ) { }
 
   ngOnInit(): void {
+  }
+
+  addEmployee(form: NgForm){
+    this.employeeService.postEmployee(form.value).subscribe(res => {
+      this.resetForm(form);
+      M.toast({html: "Save Succesfuly"});
+    })
+  }
+
+  resetForm(form?: NgForm){
+    if(form){
+      form.reset();
+      this.employeeService.selectedEmployee = new Employee();
+    }
+      
   }
 
 }
